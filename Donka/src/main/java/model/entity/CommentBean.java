ab73d07074ac1ddb177b6a9f445c6d5bcf80be6d
emtech-user <emtech-user@PC-number>
2025-05-27 15:33:42 +0900

    3回目_中静_CommentBean、VoteBean

--- Donka/src/main/java/model/entity/CommentBean.java
+++ Donka/src/main/java/model/entity/CommentBean.java
@@ -1,5 +1,39 @@
 package model.entity;
 
-public class CommentBean {
+import java.io.Serializable;
 
+public class CommentBean implements Serializable{
+	private int commentId; // コメント識別ID
+	private String content; // コメントの中身
+	private String userId; // ユーザID
+	
+	public CommentBean() {
+		
+	}
+	
+	public int getCommentId() {
+		return commentId;
+	}
+	
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
 }
--- Donka/src/main/java/model/entity/VoteBean.java
+++ Donka/src/main/java/model/entity/VoteBean.java
@@ -1,5 +1,39 @@
 package model.entity;
 
-public class VoteBean {
+import java.io.Serializable;
 
+public class VoteBean implements Serializable{
+	private int voteId; // 評価ID
+	private String userId; // ユーザID
+	private int sakeId; // 酒ID
+	
+	public VoteBean() {
+		
+	}
+	
+	public int getVoteId() {
+		return voteId;
+	}
+	
+	public void setVoteId(int voteId) {
+		this.voteId = voteId;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public int getSakeId() {
+		return sakeId;
+	}
+	
+	public void setSakeId(int sakeId) {
+		this.sakeId = sakeId;
+	}
+	
+	
 }
