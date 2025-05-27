e49af4788c7bbfd9a19afaf4a10e1caf1bb3a023
emtech-user <emtech-user@PC-number>
2025-05-27 15:37:43 +0900

    4回目_本田_SakeEvalutionBeanの中身

--- Donka/src/main/java/model/entity/SakeEvalutionBean.java
+++ Donka/src/main/java/model/entity/SakeEvalutionBean.java
@@ -1,5 +1,34 @@
 package model.entity;
 
-public class SakeEvalutionBean {
+import java.io.Serializable;
 
+public class SakeEvalutionBean implements Serializable {
+
+	/*
+	 * 酒ID
+	 */
+	private int sakeId;
+	
+	/*
+	 * 合計評価数
+	 */
+	private int totalEvalution;
+	
+	public SakeEvalutionBean() {
+		
+	}
+	
+	public int getSakeId() {
+		return sakeId;
+	}
+	public void setSakeId(int sakeId) {
+		this.sakeId = sakeId;
+	}
+	public int getTotalEvalution() {
+		return totalEvalution;
+	}
+	public void setTotalEvalution(int totalEvalution) {
+		this.totalEvalution = totalEvalution;
+	}
+	
 }
