1706e2efbc9b1e8d1463cc47b747cc0283348e1b
emtech-user <emtech-user@PC-number>
2025-05-27 15:38:53 +0900

    3回目‗坪野‗UserBeanの作成

--- Donka/src/main/java/model/entity/UserBean.java
+++ Donka/src/main/java/model/entity/UserBean.java
@@ -1,5 +1,21 @@
 package model.entity;
 
 public class UserBean {
-
+	private String userId;
+	private String nickname;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
 }
