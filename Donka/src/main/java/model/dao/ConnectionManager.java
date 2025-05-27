65eba2e41b68be7ace2ab58486ff6bc8c0098bcf
emtech-user <emtech-user@PC-number>
2025-05-27 16:50:04 +0900

    5回目_坪野_ConnectionManagerの追記

--- Donka/src/main/java/model/dao/ConnectionManager.java
+++ Donka/src/main/java/model/dao/ConnectionManager.java
@@ -1,5 +1,34 @@
 package model.dao;
 
-public class ConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionManager {
+	private static final String URL = "jdbc:mysql://localhost:3306/ys2kdb";
+
+	/**
+	 * ユーザ
+	 */
+	private static final String USER = "YS2K";
+
+	/**
+	 * パスワード
+	 */
+	private static final String PASSWORD = "password";
+
+	/**
+	 * データベースへの接続を取得して返します。
+	 *
+	 * @return コネクション
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		// JDBCドライバの読み込み
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+
+	}
 }
