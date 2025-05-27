96c1ed2615458bb92faab05b5a791b327e295a8d
emtech-user <emtech-user@PC-number>
2025-05-27 15:32:11 +0900

    AdminBeanとBreweryBean作成　武田

--- Donka/src/main/java/model/entity/AdminBean.java
+++ Donka/src/main/java/model/entity/AdminBean.java
@@ -1,5 +1,26 @@
 package model.entity;
 
-public class AdminBean {
+import java.io.Serializable;
+
+public class AdminBean implements Serializable{
+	/*
+	 * 管理者ID
+	 */
+	private String adminId;
+	
+	/**
+	 * AdminBeanを構築
+	 */
+	public AdminBean() {
+	}
+	
+	
+	public String getAdminId() {
+		return adminId;
+	}
+	
+	public void setAdminId(String adminId) {
+		this.adminId=adminId;
+	}
 
 }
--- Donka/src/main/java/model/entity/BreweryBean.java
+++ Donka/src/main/java/model/entity/BreweryBean.java
@@ -1,5 +1,150 @@
 package model.entity;
 
-public class BreweryBean {
+import java.io.Serializable;
+
+public class BreweryBean implements Serializable{
+	
+	/*
+	 * 酒ID
+	 */
+	private String breweryId;
+	
+	/*
+	 * 酒の名前
+	 */
+	private String breweryName;
+	
+	/*
+	 * 写真のURL
+	 */
+	private String blmgPath;
+	
+	/*
+	 * 緯度
+	 */
+	private int lattitude;
+	
+	/*
+	 * 経度
+	 */
+	private int longtiude;
+	
+	/*
+	 * 住所
+	 */
+	private String address;
+	
+	/*
+	 * 味わい
+	 */
+	private Boolean reservation;
+	
+	/*
+	 * 酒蔵の説明
+	 */
+	private String breweryExplanation;
+	
+	/*
+	 * 地域ID
+	 */
+	private int areaId;
+	
+	
+	/*
+	 * BrewreyBeanを構築
+	 */
+	public BreweryBean() {
+		
+	}
+
+
+	public String getBreweryId() {
+		return breweryId;
+	}
+
+
+	public void setBreweryId(String breweryId) {
+		this.breweryId = breweryId;
+	}
+
+
+	public String getBreweryName() {
+		return breweryName;
+	}
+
+
+	public void setBreweryName(String breweryName) {
+		this.breweryName = breweryName;
+	}
+
+
+	public String getBlmgPath() {
+		return blmgPath;
+	}
+
+
+	public void setBlmgPath(String blmgPath) {
+		this.blmgPath = blmgPath;
+	}
+
+
+	public int getLattitude() {
+		return lattitude;
+	}
+
+
+	public void setLattitude(int lattitude) {
+		this.lattitude = lattitude;
+	}
+
+
+	public int getLongtiude() {
+		return longtiude;
+	}
+
+
+	public void setLongtiude(int longtiude) {
+		this.longtiude = longtiude;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public Boolean getReservation() {
+		return reservation;
+	}
+
+
+	public void setReservation(Boolean reservation) {
+		this.reservation = reservation;
+	}
+
+
+	public String getBreweryExplanation() {
+		return breweryExplanation;
+	}
+
+
+	public void setBreweryExplanation(String breweryExplanation) {
+		this.breweryExplanation = breweryExplanation;
+	}
+
+
+	public int getAreaId() {
+		return areaId;
+	}
+
+
+	public void setAreaId(int areaId) {
+		this.areaId = areaId;
+	}
 
 }
