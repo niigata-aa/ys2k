8130741e75bfa75ad7cadaf3ff5c273fc657a706
emtech-user <emtech-user@PC-number>
2025-05-29 09:05:20 +0900

    10回目_本田_武田さんのやつ

--- Donka/src/main/java/servlet/BreweryViewServlet.java
+++ Donka/src/main/java/servlet/BreweryViewServlet.java
@@ -1,12 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.BreweryDAO;
+import model.entity.BreweryBean;
 
 /**
  * Servlet implementation class BreweryViewServlet
@@ -28,15 +34,29 @@ public class BreweryViewServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		List<BreweryBean> breweryList=null;
+		
+	
+		BreweryDAO dao=new BreweryDAO();
+		
+		try {
+			breweryList=dao.selectAll();
+		}catch(SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("breweryList", breweryList);
+		
+		RequestDispatcher rd=request.getRequestDispatcher("breweryList.jsp");
+		rd.forward(request, response);
 	}
 
 }
--- Donka/src/main/webapp/breweryList.jsp
+++ Donka/src/main/webapp/breweryList.jsp
@@ -1,12 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="java.util.List,model.entity.BreweryBean"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>酒蔵一覧</title>
 </head>
 <body>
+<%
+List<BreweryBean> breweryList=(List<BreweryBean>)request.getAttribute("breweryList");
+%>
+<%
+for(BreweryBean brewery:breweryList){
+%>	
+<%=brewery.getBreweryName() %>
+<img src="<%=brewery.getBImgPath() %>"><br>
+
+<%
+}
+%>
+
+
+
 
 </body>
 </html>
