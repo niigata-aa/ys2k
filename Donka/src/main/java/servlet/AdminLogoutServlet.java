14d87683d1c19560dacfb2867c82448c54be9e6a
emtech-user <emtech-user@PC-number>
2025-06-04 17:04:08 +0900

    28回目武田　いらないもの削除

--- Donka/src/main/java/servlet/AdminLogoutServlet.java
+++ Donka/src/main/java/servlet/AdminLogoutServlet.java
@@ -35,8 +35,7 @@ public class AdminLogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
